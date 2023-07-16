4552400e7bbd7375a718bd3a6291b67e620a48cc
Lactor <lactorjm4@gmail.com>
2023-07-16 19:09:46 +0300

    Completed Assingment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats=["Milo", "Otis", "Garfield"]
+
+  function destructivelyAppendCat(name){
+    cats.push(name);
+  }
+  
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+
+  function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+  }
+  
+  function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+  }
+
+  function appendCat(name){
+    return [...cats, "Broom"];
+  }
+
+  function prependCat(name){
+    return ["Arnold", ...cats];
+  }
+
+  function removeLastCat(name){
+    const catsCopy = [...cats];     
+    catsCopy.pop();
+    return catsCopy;  
+  }
+
+  function removeFirstCat(name){
+    const catsCopy = [...cats];
+    catsCopy.shift();
+    return catsCopy;
+  }
